bb5da0dc3f50b93693cd8f051ae8c5fd23ab97ce
MarojT <celta0992@hotmail.com>
2022-10-15 14:02:06 +0700

    Add stdev test.

--- test_statistics.py
+++ test_statistics.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from statistics import variance
+from statistics import variance, stdev
 
 
 class StatisticsTest(TestCase):
@@ -14,6 +14,13 @@ class StatisticsTest(TestCase):
         """variance of an empty list should raise an exception"""
         with self.assertRaises(ValueError):
             var = variance([])
+
+    def test_stdev(self):
+        """test values for standard deviation."""
+        data = [5.0, 5.0]
+        self.assertEqual(0.0, stdev(data))
+        data = [5.0, 10.0]
+        self.assertEqual(2.5, stdev(data))
 
 
 if __name__ == '__main__':
